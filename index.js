f05928f2ac2b3a888fdaf4a50db4a3203da41fc7
Erfan shamsaldini <erfanshamsaldini@gmail.com>
2022-09-18 12:12:14 -0400

    feat: new feature

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const { WorkOS } = require('@workos-inc/node');
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
- })
- 
+
+const app = express();
+
+
+const workos = new WorkOS(process.env.WORKOS_API_KEY);
+const clientID = process.env.WORKOS_CLIENT_ID;
+
+
+app.get('/auth', (_req, res) => {
+  const connection = 'connection_123'; // The Connection's ID
+  const redirectURI = 'https://dashboard.my-app.com'; // The callback URI WorkOS should redirect to post-authentication
+
+
+  const authorizationURL = workos.sso.getAuthorizationURL({
+    connection,
+    clientID,
+    redirectURI,
+  });
+
+
+  res.redirect(authorizationURL);
+});
  
